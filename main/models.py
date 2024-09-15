71a812ff750fb21ab025ba12d9823c29cac41880
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-09-15 20:52:22 +0700

    update

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
+import uuid
 from django.db import models
 
+
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     coquetteness = models.TextField()
 
     def __str__(self):
-        return self.name
+         return self.name
